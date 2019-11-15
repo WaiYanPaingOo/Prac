8878c3dfecb470e49e287c665543da980172fc62
Wai Yan Paing Oo <waiyanpaingoo@my.jcu.edu.au>
2019-11-15 11:48:31 +0800

    "Fixed Debugs"

---
+++ src/prac_01/DebuggingExercise/FixDebugOne4.java
@@ -0,0 +1,11 @@
+package prac_01.DebuggingExercise;
+
+import javax.swing.JOptionPane;
+
+public class FixDebugOne4
+{
+  public static void main(String[]args)
+  {
+     JOptionPane.showMessageDialog(null, "First GUI program");
+  }
+}
